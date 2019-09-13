ab541b31cbce4d5c97e079721ec11c3456b03846
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2019-09-13 17:47:05 -0500

    Upload Elevator 1 Homework

---
+++ CPlusPlusElevator_v3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int floorOn = 0;
+    int floorsMoved = 0;
+    int floorTo = 0;
+
+    printf("Welcome! You are on floor %d.\n", floorOn);
+    printf("Please select what floor you would like to be on (0-10).");
+    scanf("%d", &floorTo); //saw sscanf from here https://www.asc.ohio-state.edu/physics/ntg/780/handouts/interactive_input_in_C.pdf
+    //not sure if it's different from just scanf()
+    //sscanf() is different from scanf() it seems.
+
+    if(floorTo == 0 || floorTo == 1 || floorTo == 2 || floorTo == 3 || floorTo == 4 || floorTo == 5 || floorTo == 6 || floorTo == 7 || floorTo == 8 || floorTo == 9 || floorTo == 10)
+    {
+        floorsMoved = floorOn+floorTo;
+        floorOn = floorTo;
+        printf("You have moved to floor %d, and have moved %d floors.\n", floorOn, floorsMoved);
+        printf("You have been to floors 0 and %d", floorOn);
+    }
+    else
+    {
+        printf("That is not a valid floor");
+    }
+}
