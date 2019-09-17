3b1c90ea909311bdb90fdd62cb13aa7e82308860
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2019-09-17 15:59:02 -0500

    Adds comment about error

--- CPlusPlusElevator_v3/main.c
+++ CPlusPlusElevator_v3/main.c
@@ -11,6 +11,7 @@ int main()
     scanf("%d", &floorTo); //saw sscanf from here https://www.asc.ohio-state.edu/physics/ntg/780/handouts/interactive_input_in_C.pdf
     //not sure if it's different from just scanf()
     //sscanf() is different from scanf() it seems.
+    //scanf() brings up an error that I'm not sure is actually an error. The error suggestion says to use _CRT_SECURE_NO_WARNINGS which seems to be just more of a way to ignore the error, which I don't think you want.
 
     if(floorTo == 0 || floorTo == 1 || floorTo == 2 || floorTo == 3 || floorTo == 4 || floorTo == 5 || floorTo == 6 || floorTo == 7 || floorTo == 8 || floorTo == 9 || floorTo == 10)
     {
